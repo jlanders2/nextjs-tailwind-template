11677bebc44d87174f44bac56784844779f4c569
John Cole Landers <jclanders@protonmail.com>
2022-05-12 01:36:52 -0500

    most of the stuff here was just to setup dev tooling

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <p className="text-3xl font-bold underline">Test</p>
         </h1>
 
         <p className={styles.description}>
@@ -65,5 +67,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
